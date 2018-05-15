113a9811d6557497b36eefa0a519ca1ff388ccf3
ruslangm88 <ruslan.guerra88@gmail.com>
2018-05-14 22:22:38 -0300

    updating translation

--- src/assets/js/translation.js
+++ src/assets/js/translation.js
@@ -221,9 +221,9 @@ var Translation = {
             "EN": "You must provide all data" 
         },
 "Você não possui leads que cumplam com sua solicitude": 
-        {   "PT": "Você não possui leads pagos que cumplam com sua solicitude",
-            "ES": "Usted no tiene leads pagados que cumplan con su solicitud",
-            "EN": "You have no paid leads that match your request." 
+        {   "PT": "Você não possui leads pagos no periodo solicitado",
+            "ES": "Usted no tiene leads pagados en el periodo solicitado",
+            "EN": "You have no paid leads in the requested period." 
         },
 "Ooops ... problema no servidor": 
         {   "PT": "Ooops ... problema no servidor",
