aaeeec96c20aab45a0105dcdbae3aa8554207871
jorgeFedora <jorge85.mail@gmail.com>
2018-05-22 16:24:52 -0300

    all brazilian

--- src/application/controllers/Welcome.php
+++ src/application/controllers/Welcome.php
@@ -48,6 +48,7 @@ class Welcome extends CI_Controller {
         if($_SERVER['REMOTE_ADDR'] === "191.252.100.122")
             return 1;
         
+        return 1;
         return 0;
 //        $datas = file_get_contents('https://ipstack.com/ipstack_api.php?ip='.$_SERVER['REMOTE_ADDR']);//
 //        $response = json_decode($datas);
